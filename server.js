095ecad95d8196b18bc2964d0faa9802407a0663
Michael Antczak <Michael.Antczak@gmail.com>
2017-08-17 11:03:01 +0100

    Initial commit
    Starting point is the Express workshop 2 from CYF repo

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+// The extensions 'html' allows us to serve file without adding .html at the end 
+// i.e /my-cv will server /my-cv.html
+app.use(express.static("public", {'extensions': ['html']}));
+
+
+// what does this line mean: process.env.PORT || 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
