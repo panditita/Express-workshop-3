415c05ac037276cb4381b4cb5ea866a7523f81ee
Michael Antczak <Michael.Antczak@gmail.com>
2017-08-17 11:46:18 +0100

    Add fetching posts from local file system

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
+const fs = require('fs');
 
 // Then these two lines after you initialise your express app 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -11,10 +12,20 @@ app.set('view engine', 'handlebars');
 app.use(express.static("public", {'extensions': ['html']}));
 
 app.get('/', function (req, res) {
-    res.render('index', {
-      title: "Michael's profile",
-      subheading: "A modern Website built in Node with Handlebars",
-    });
+    const filePath = __dirname + '/data/posts.json';
+    const callbackFunction = function(error, file) {
+        // we call .toString() to turn the file buffer to a String
+        const fileData = file.toString();
+        // we use JSON.parse to get an object out the String
+        const postsJson = JSON.parse(fileData);
+        // send the json to the Template to render
+        res.render('index', {
+          title: "Michael's profile",
+          subheading: "A modern Website built in Node with Handlebars",
+          posts: postsJson
+        });
+    };
+    fs.readFile(filePath, callbackFunction);
 });
 
 app.get('/my-cv', function (req, res) {
