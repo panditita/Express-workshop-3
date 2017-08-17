c57afe3e3ac403b732e855fc0e697f00caf836ed
Michael Antczak <Michael.Antczak@gmail.com>
2017-08-17 11:18:45 +0100

    Add Handlebars
    Remove the index file from public folder, so hbs can be served

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const exphbs  = require('express-handlebars');
+
+// Then these two lines after you initialise your express app 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // The extensions 'html' allows us to serve file without adding .html at the end 
 // i.e /my-cv will server /my-cv.html
 app.use(express.static("public", {'extensions': ['html']}));
 
+app.get('/', function (req, res) {
+    res.render('index');
+});
 
 // what does this line mean: process.env.PORT || 3000
 app.listen(process.env.PORT || 3000, function () {
